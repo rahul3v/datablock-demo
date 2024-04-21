a73a17e298d985954ea2b76596845a538ada7ca0
Rahul Raj <rahul3v@users.noreply.github.com>
2024-04-22 01:38:43 +0530

    Update HistoryUi.tsx

--- components/appui/HistoryUi.tsx
+++ components/appui/HistoryUi.tsx
@@ -1,6 +1,6 @@
 import { Redo, Undo } from "lucide-react";
 
-const buttonStyle = `flex gap-1 cursor-not-allowed  items-center px-3 py-1 rounded-xl border-violet-950 z-10 bg-indigo-800 cursor-pointer opacity-80 duration-200 hover:opacity-100 shadow`
+const buttonStyle = `flex gap-1 cursor-not-allowed  items-center px-3 py-1 rounded-xl border-violet-950 z-10 bg-indigo-800 opacity-80 duration-200 hover:opacity-100 shadow`
 
 export const HistoryUi = () => {
   return <div className="flex gap-2 opacity-35">
