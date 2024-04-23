69e67e0081271418dbe712479ef95b0ab6d6a2a7
Rahul Raj <rahul3v@users.noreply.github.com>
2024-04-24 01:00:17 +0530

    Adding batter styling

--- components/blockui/Template.tsx
+++ components/blockui/Template.tsx
@@ -2,7 +2,7 @@ import { DeleteButton } from '@/components/blockui/DeleteButton';
 
 export function BlockTemplate({ id, type, label, children }: { id: string, type: string, label: string, children: JSX.Element }) {
   return (
-    <div className="file-block px-2 py-2 bg-white text-black rounded-sm relative" data-block={type}>
+    <div className="file-block px-2 py-2 bg-white text-black rounded-lg relative" data-block={type}>
       <div className="mb-2 flex justify-between border-b-2 text-[10px]">
         <b>{label}</b>
         <DeleteButton id={id} />
