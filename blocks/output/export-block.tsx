de322e5eaf86fab5904094eaae2c8873363a3713
Rahul Raj <rahul3v@users.noreply.github.com>
2024-04-24 03:40:16 +0530

    Adding Export block

--- blocks/output/export-block.tsx
+++ blocks/output/export-block.tsx
@@ -1,20 +1,43 @@
-import { convertToCSV } from "@/lib/data-block.lib"
+import { exportCsvData, exportJsonData } from "@/lib/data-block.lib"
 import { memo } from "react"
-import { Handle, Position } from "reactflow"
+import { Position, useEdges, useNodes } from "reactflow"
 import BlockTemplate from "@/components/blockui/Template";
 import CustomHandle from "@/components/blockui/CustomHandle";
 
 const buttonStyle = `px-3 py-1 w-full rounded-xl text-white bg-orange-400	cursor-pointer text-[10px]`
 
 function ExportBlock({ id }: { id: string }) {
+  const edges = useEdges();
+  const nodes = useNodes()
+
+  function getDataset(type: 'csv' | 'json') {
+    const edge = edges.find(edge => edge.target == id)
+    if (!edge) {
+      alert('No Data found, Connect to your data source')
+      return
+    }
+    const node = nodes.find(node => node.id == edge?.source)
+    //@ts-ignore
+    if (node?.data && node.data['fileData']) {
+      //@ts-ignore
+      const dataset = node.data['fileData']
+      if (type === 'csv') {
+        exportCsvData(dataset, 'datset-file-' + Date.now())
+      }
+      if (type === 'json') {
+        exportJsonData(dataset, 'datset-file-' + Date.now())
+      }
+    }
+
+  }
 
   return <BlockTemplate id={id} label="Export Data" type={"exportfile"}>
     <>
       <div className="flex flex-col gap-4">
-        <button className={buttonStyle}>Export CSV</button>
-        <button className={buttonStyle}>Export JSON</button>
+        <button className={buttonStyle} onClick={() => { getDataset("csv") }}>Export CSV</button>
+        <button className={buttonStyle} onClick={() => { getDataset("json") }}>Export JSON</button>
       </div>
-      <CustomHandle connectionLimit={1} type="target" position={Position.Left} id={id} />
+      <CustomHandle connectionLimit={1} type="target" acceptType={["filter", "filepicker"]} position={Position.Left} id={id} />
     </>
   </BlockTemplate>
 }
