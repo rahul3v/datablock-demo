ec55e6165ed1a38a7455f388f3d4f685821b512f
Rahul Raj <rahul3v@users.noreply.github.com>
2024-04-23 19:21:17 +0530

    Adding connection limit to few blocks

--- blocks/output/export-block.tsx
+++ blocks/output/export-block.tsx
@@ -2,6 +2,7 @@ import { convertToCSV } from "@/lib/data-block.lib"
 import { memo } from "react"
 import { Handle, Position } from "reactflow"
 import BlockTemplate from "@/components/blockui/Template";
+import CustomHandle from "@/components/blockui/CustomHandle";
 
 const buttonStyle = `px-3 py-1 w-full rounded-xl text-white bg-orange-400	cursor-pointer text-[10px]`
 
@@ -13,7 +14,7 @@ function ExportBlock({ id }: { id: string }) {
         <button className={buttonStyle}>Export CSV</button>
         <button className={buttonStyle}>Export JSON</button>
       </div>
-      <Handle type="target" position={Position.Left} id={id} />
+      <CustomHandle connectionLimit={1} type="target" position={Position.Left} id={id} />
     </>
   </BlockTemplate>
 }
